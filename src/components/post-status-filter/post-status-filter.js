d2cb01767a0211049f08051ed20e88ba0fd9692a
Aleksandr <aleck.ostapenko@gmail.com>
2020-09-11 17:42:18 +0300

    Working with forms

--- src/components/post-status-filter/post-status-filter.js
+++ src/components/post-status-filter/post-status-filter.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Button } from 'reactstrap';
 import './post-status-filter.css';
 
-const PostStatusFilter = () => {
-    return (
-        <div className="btn-group">
-            <Button color='info'>Все</Button>
-            <button type='button' className="btn btn-info">Все</button>
-            <button type='button' className="btn btn-outline-secondary">Понравилось</button>
-        </div>
-    )
+export default class PostStatusFilter extends Component {
+    buttons = [
+        {name: 'all', label: 'Все'},
+        {name: 'like', label: 'Понравилось'}
+    ]
+    
+    render() {
+        const buttons = this.buttons.map(({name, label}) => {
+            const {filter, onFilterSelect} = this.props; 
+            const active = filter === name,
+                  clazz = active ? 'primary' : 'outline-primary'
+            return (
+                <Button key={name} color={clazz}
+                 onClick={() => onFilterSelect(name)}>{label}</Button>
+            )
+        });
+        
+        return (
+            <div className="btn-group">
+                {buttons}
+            </div>
+        )
+    }
 }
-
-export default PostStatusFilter;
--- src/index.js
+++ src/index.js
@@ -4,71 +4,9 @@ import './index.css';
 import App from './components/app/';
 import 'bootstrap/dist/css/bootstrap.css';
 
-/*class WhoAmI extends React.Component{
-  constructor (props) {
-    super(props);
-    this.state={
-      years: 26
-    }
-    //this.nextYear = this.nextYear.bind(this); 
-    this.nextYear=() => {
-      this.setState(state => ({
-         years: state.years++
-      }))  
-    }
-  } */
-
-  // state={
-  //   years: 26
-  // }
-
-  // nextYear = () => {
-  //     this.setState(state => ({
-  //       years: state.years++
-  //     }))
-  //   }    
-  // nextYear() {
-  //   this.setState(state => ({
-  //     years: state.years++
-  //   }))
-  // }
-  
- /* render () {
-    const {name,surname,link} = this.props;
-    const {years} = this.state;
-    return (
-      <>
-        <button onClick={this.nextYear}>++</button>
-        <h1>My name is {name}, surname - {surname}, years = {years}</h1>
-        <a href={link}>My profile</a>
-      </>
-    )
-  }
-} 
-
-
-const All = () => {
-  
-  return (
-    <>
-      <WhoAmI name="Joann" surname="Smitti" link="facebook.com" />
-      <WhoAmI name="Ivan" surname="Popov" link="vk.com" />
-      <WhoAmI name="Conan" surname="Varvar" link="youtube.com" />  
-    </>
-  )
-}*/
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <All />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
