da72346c88b345ab746366a2c522b6aad03904ac
Aleksandr <aleck.ostapenko@gmail.com>
2021-06-15 19:14:39 +0300

    Updates

---
+++ precache-manifest.f441f861d4583956770f4d9396013b88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c8ccd268559cdf98ea6ed438c11be3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51dfd3b2d29e6dfadf92",
+    "url": "/static/css/2.32420154.chunk.css"
+  },
+  {
+    "revision": "c4aab88decd8779466cb",
+    "url": "/static/css/main.558c40db.chunk.css"
+  },
+  {
+    "revision": "51dfd3b2d29e6dfadf92",
+    "url": "/static/js/2.6c15d260.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6c15d260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4aab88decd8779466cb",
+    "url": "/static/js/main.11a094cd.chunk.js"
+  },
+  {
+    "revision": "076103b3bf56b075711f",
+    "url": "/static/js/runtime-main.87226f1e.js"
+  }
+]);
